b1cee6c3093fe3fec23ed3e2ba6b5bf61303bac5
mkylychev <mkylychev@gmail.com>
2017-07-22 01:27:18 +0600

    create playSound method in Media.class

--- core/src/kg/lunar/driver/media/Media.java
+++ core/src/kg/lunar/driver/media/Media.java
@@ -1,6 +1,18 @@
 package kg.lunar.driver.media;
 
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Sound;
 
 public class Media {
+    private AssetManager assetManager;
+
+    public Media(AssetManager assetManager) {
+        this.assetManager = assetManager;
+    }
+
+    public void playSound(String name){
+        Sound sound = assetManager.get("sounds/"+name,Sound.class);
+        sound.play();
+    }
 }
