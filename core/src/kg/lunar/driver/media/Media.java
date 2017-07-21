96625bfaad90296f7c949c6dd9858b50d770c8a1
mkylychev <mkylychev@gmail.com>
2017-07-22 01:33:18 +0600

    create playMusic, stopMusic, addMusic, removeMusic methods in Media.class

--- core/src/kg/lunar/driver/media/Media.java
+++ core/src/kg/lunar/driver/media/Media.java
@@ -2,6 +2,7 @@ package kg.lunar.driver.media;
 
 
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 
 public class Media {
@@ -11,8 +12,27 @@ public class Media {
         this.assetManager = assetManager;
     }
 
-    public void playSound(String name){
-        Sound sound = assetManager.get("sounds/"+name,Sound.class);
+    public void playSound(String name) {
+        Sound sound = assetManager.get("sounds/" + name, Sound.class);
         sound.play();
+    }
+
+    public void playMusic(String name, boolean loop) {
+        Music music = assetManager.get("musics/" + name, Music.class);
+        music.setLooping(loop);
+        music.play();
+    }
+    public void stopMusic(String name) {
+        Music music = assetManager.get("musics/" + name, Music.class);
+        music.stop();
+    }
+    public void addMusic(String name){
+        assetManager.load("musics/"+name,Music.class);
+    }
+    public void removeMusic(String name){
+        assetManager.unload("musics/"+name);
+    }
+    public boolean update(){
+        return assetManager.update();
     }
 }
